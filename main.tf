749d78d3c2147715214621a0cac34140c659f299
Sang Venkatraman <1299079+sangv@users.noreply.github.com>
2019-01-29 10:28:22 -0500

    Changes for ELB

--- main.tf
+++ main.tf
@@ -74,26 +74,89 @@ resource "aws_security_group" "instance" {
   }
 }
 
+resource "aws_security_group" "elb" {
+  name        = "terraform-example-elb"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # for health checks
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
 variable "server_port" {
   description = "The port that will be used to serve HTTP requests"
   default = 8080
 }
 
-resource "aws_instance" "example" {
-	ami = "ami-40d28157"
+data "aws_availability_zones" "all" {}
+
+resource "aws_launch_configuration" "example" {
+	image_id = "ami-40d28157"
 	instance_type = "t2.micro"
-	vpc_security_group_ids = ["${aws_security_group.instance.id}"]
-	subnet_id = "${aws_subnet.public.id}"
-  tags {
-    Name = "terraform-example"
-  }
+	security_groups = ["${aws_security_group.instance.id}"]
+	#subnet_id = "${aws_subnet.public.id}"
+  
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  #["${data.aws_availability_zones.all.names}"] - Hard coding because classic load balancers are not supported in some  AZ's
+  availability_zones = ["us-east-1a","us-east-1b"]
+  load_balancers = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 3
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  
+  #["${data.aws_availability_zones.all.names}"] - Hard coding because classic load balancers are not supported in some  AZ's
+  availability_zones = ["us-east-1a","us-east-1b"]
+  
+  #security_groups = ["${aws_security_group.elb.id}"]
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = "${var.server_port}"
+    instance_protocol = "http"
+  }
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+  }
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
 }

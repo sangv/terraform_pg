058414146ee20cc5342a769d67b077529fb55ee7
Sang Venkatraman <1299079+sangv@users.noreply.github.com>
2019-01-29 07:25:06 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,73 @@
+provider "aws" {
+	region = "us-east-1"
+}
+
+#create a dedicated vpc
+resource "aws_vpc" "main" {
+        cidr_block = "10.0.0.0/16"
+}
+
+# Create an internet gateway to give our subnet access to the outside world
+resource "aws_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.main.id}"
+}
+
+# Grant the VPC internet access on its main route table
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.main.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.default.id}"
+}
+
+# Create a subnet to launch our instances into
+resource "aws_subnet" "public" {
+  vpc_id                  = "${aws_vpc.main.id}"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+
+# Our default security group to access
+# the instances over SSH and HTTP
+resource "aws_security_group" "default" {
+  name        = "terraform_example"
+  description = "Used in the terraform"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from the VPC
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#https://github.com/terraform-providers/terraform-provider-aws/blob/master/examples/two-tier/main.tf
+
+resource "aws_instance" "example" {
+	ami = "ami-40d28157"
+	instance_type = "t2.micro"
+	vpc_security_group_ids = ["${aws_security_group.default.id}"]
+	subnet_id = "${aws_subnet.public.id}"
+  tags {
+    Name = "terraform-example"
+  }
+}
+
